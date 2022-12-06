ab9a072374845db45bce89d935e0a56c5db3985c
hsmtkk <hsmtkk@gmail.com>
2022-12-06 12:51:40 +0900

    add debug code

--- function/function.go
+++ function/function.go
@@ -237,6 +237,11 @@ func downloadImage(channelAccessToken, imageID string) ([]byte, error) {
 		return nil, fmt.Errorf("http.DefaultClient.Get failed; %w", err)
 	}
 	defer resp.Body.Close()
+
+	// debug
+	dumpResp, err := httputil.DumpResponse(resp, true)
+	log.Printf("dump response: %s", string(dumpResp))
+
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("non 200 HTTP status code; %d; %s", resp.StatusCode, resp.Status)
 	}
